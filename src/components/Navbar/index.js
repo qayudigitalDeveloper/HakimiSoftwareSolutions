fbd6f3e10f477989336bb2abf2b7e1f9c238b9e0
tejaswi kurapati <tejaswikurapati88@gmail.com>
2025-07-19 18:42:44 +0530

    updated nav with contact details

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,10 @@
 import './index.css';
 import logo from './../../assets/logo1.png';
 import { useEffect, useRef, useState } from 'react';
+import { FaPhoneAlt } from "react-icons/fa";
+import { GoMail } from "react-icons/go";
+import { FaLinkedinIn } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
 
 const navContent = [
   { id: 1, text: 'Home', index: '#home' },
@@ -12,11 +16,13 @@ const navContent = [
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(true);
+  const [atTop, setAtTop] = useState(true);
   const lastScrollY = useRef(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
+      setAtTop(window.scrollY === 0);
 
       if (currentScrollY < lastScrollY.current) {
         setShowNavbar(true); // scrolling up
@@ -38,43 +44,45 @@ const Navbar = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const style = {
-    display: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    paddingLeft: "110px",
-    backgroundColor: "#FFFFFF"
-  }
-
   return (
     <div >
-      <div className='nav-up-cont'>
-        <h1>Top nav</h1>
-      </div>
-      <div className={`outer-cont ${showNavbar ? 'visible' : 'hidden'}`}>
+      {atTop && (
+        <div className='nav-up-cont'>
+          <div style={{display:"flex", alignItems: 'center', width: "20%", justifyContent:"space-between"}}>
+            <p><FaPhoneAlt /> +971-566-601-500 </p>
+            <p><GoMail /> info@qayudigital.com</p>
+          </div>
+          <div style={{display:"flex", alignItems: 'center', width: "20%", justifyContent:"space-between"}}>
+            <GoMail style={{fontSize: "23px"}}/>
+            <FaLinkedinIn style={{fontSize: "23px"}} />
+            <IoLogoInstagram  style={{fontSize: "23px"}}/>
+          </div>
+        </div>
+      )}
+      <div className={`outer-cont ${showNavbar ? 'visible' : 'hidden'} ${atTop === true && "applymargintop"}`} >
         <div className='in-cont-nav'>
           <div className='img-cont-nav'>
-        <img className='logo' onClick={scrollToTop} src={logo} alt='logo' /> 
-        <p  className='text-img'>
-        ZOHO Expertise
-      </p>
-      </div>
-        <div className='nav-sub-cont'>
-          <ul className='ul-nav'>
-            {navContent.map(each => (
-              <li className='li-nav' key={each.id}>
-                <a href={each.index}>
-                  <p className='nav-text underline-animate'>{each.text}</p>
-                </a>
-              </li>
-            ))}
-          </ul>
+            <img className='logo' onClick={scrollToTop} src={logo} alt='logo' />
+            <p className='text-img'>
+              ZOHO Expertise
+            </p>
+          </div>
+          <div className='nav-sub-cont'>
+            <ul className='ul-nav'>
+              {navContent.map(each => (
+                <li className='li-nav' key={each.id}>
+                  <a href={each.index}>
+                    <p className='nav-text underline-animate'>{each.text}</p>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <a href="#footer"><h1 className='big-text'>Contact Us</h1></a>
         </div>
-        <a href="#footer"><h1 className='big-text'>Contact Us</h1></a>
-        </div>
-        
+
       </div>
-      
+
     </div>
   );
 };
